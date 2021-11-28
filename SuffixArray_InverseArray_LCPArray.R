5849d3b505c53f4ac4a317389d80533a517b4d4c
Anna Kadlubcova <xkadlu02@vutbr.cz>
2021-11-28 16:29:10 +0100

    Vytvoreni suffixu, ale nejde zakomponovat DNAString().

--- SuffixArray_InverseArray_LCPArray.R
+++ SuffixArray_InverseArray_LCPArray.R
@@ -1,14 +1,31 @@
-SuffixArray <- function(word){
-  L <- length(word)
-  for (k in 1:L){
+# cviko 8, 25.11.2021
+################################################################################
+######  SUFFIX STROMY  #########################################################
 
+# library(Biostrings)
+# word <- DNAString("GCATCAT.")
+# pattern = DNAString("CAT")
+word <- 'GCATCAT$'
+pattern = 'CAT'
+
+SuffixArray <- function(word){
+  L <- nchar(word)
+  #L <- length(word)
+  suffixy <- matrix(data=0, nrow=L, ncol=1)
+  #SA <- c()
+  
+  for (k in 1:L){
+    suffixy[k,1] <- substring(word, k, L)
+    #SA <- SA[c(SA, DNAStringSet(word, k, L))]
   }
+  # potreba seradit suffixy abecedne
   return(SA)
 }
 
 InverseArray <- function(SA){
+  ISA <- matrix(data=0, nrow=L, ncol=1)
   for (k in 1:L){
-
+    ISA[k,1] <- 
   }
   return(ISA)
 }
